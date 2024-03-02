202e4f0ff39eed5fc8bf124ee3cbb3281889bec9
Eric Negron <esnegron@icloud.com>
2024-03-02 17:49:41 -0500

    Initial Commit

---
+++ GHFollowers_UIKitUITests/GHFollowers_UIKitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GHFollowers_UIKitUITestsLaunchTests.swift
+//  GHFollowers_UIKitUITests
+//
+//  Created by Eric Negron on 3/2/24.
+//
+
+import XCTest
+
+final class GHFollowers_UIKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
