0dcf945950a8de84acc8605ab06517b2955aa1cc
Eric Negron <esnegron@icloud.com>
2024-03-03 11:10:12 -0500

    added custom text field; added custom uibutton; added directories for models and views

---
+++ GHFollowers_UIKit/GFButton.swift
@@ -0,0 +1,37 @@
+//
+//  GFButton.swift
+//  GHFollowers_UIKit
+//
+//  Created by Eric Negron on 3/3/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        // custom setup
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configureButton()
+    }
+    
+    // MARK: custom styling
+    private func configureButton() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
---
+++ GHFollowers_UIKit/GFTextField.swift
@@ -0,0 +1,43 @@
+//
+//  GFTextField.swift
+//  GHFollowers_UIKit
+//
+//  Created by Eric Negron on 3/3/24.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureTextField()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    // MARK: Custom styling
+    private func configureTextField() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter A Username"
+    }
+}
