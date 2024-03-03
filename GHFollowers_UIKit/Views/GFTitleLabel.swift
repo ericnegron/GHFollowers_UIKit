180096df99b246f8d61d155769b789dc552d2206
Eric Negron <esnegron@icloud.com>
2024-03-03 14:21:17 -0500

    added custom title label styling

---
+++ GHFollowers_UIKit/Views/GFTitleLabel.swift
@@ -0,0 +1,37 @@
+//
+//  GFTitleLabel.swift
+//  GHFollowers_UIKit
+//
+//  Created by Eric Negron on 3/3/24.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureLabel()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configureLabel()
+    }
+    
+    // MARK: Custom Styling
+    private func configureLabel() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
