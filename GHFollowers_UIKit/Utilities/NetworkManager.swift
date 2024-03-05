d57dae9a1f6bec03f469fe38b6bb0b07af1681ac
Eric Negron <esnegron@icloud.com>
2024-03-05 11:42:17 -0500

    added basic image caching

--- GHFollowers_UIKit/Utilities/NetworkManager.swift
+++ GHFollowers_UIKit/Utilities/NetworkManager.swift
@@ -5,12 +5,13 @@
 //  Created by Eric Negron on 3/3/24.
 //
 
-import Foundation
+import UIKit
 
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/"
+    private let baseURL = "https://api.github.com/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
